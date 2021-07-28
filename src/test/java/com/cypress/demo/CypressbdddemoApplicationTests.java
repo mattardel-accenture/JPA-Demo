f1baee227c801d1669514a75ee34059f53f09e63
Aiizad Kulchoroeva <aiizad.kulchoroeva@accenture.com>
2021-07-28 14:58:26 -0400

    Initial commit

---
+++ src/test/java/com/cypress/demo/CypressbdddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cypress.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypressbdddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
