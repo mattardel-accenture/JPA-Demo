eaa08939cc8b7f1ac83541a78bd124b4505fd3d1
Aiizad Kulchoroeva <aiizad.kulchoroeva@accenture.com>
2021-08-03 10:27:56 -0400

    added my favorites

--- src/main/java/com/cypress/demo/GreetingController.java
+++ src/main/java/com/cypress/demo/GreetingController.java
@@ -9,10 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @GetMapping("/favorites")
+    public String favorites(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "favorites";
+    }
+
+    @GetMapping("/bookstore")
+    public String bookstore(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "bookstore";
     }
 
 }
