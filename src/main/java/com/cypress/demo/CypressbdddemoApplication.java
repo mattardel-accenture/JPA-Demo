e2c9d306a801e18db89019a1b466b0f70ca56fa3
Aiizad Kulchoroeva <aiizad.kulchoroeva@accenture.com>
2021-08-12 14:57:45 -0400

    connected to h2-non- memory database

--- src/main/java/com/cypress/demo/CypressbdddemoApplication.java
+++ src/main/java/com/cypress/demo/CypressbdddemoApplication.java
@@ -17,38 +17,19 @@ public class CypressbdddemoApplication {
 	@Bean
 	public CommandLineRunner init(BookRepository bookRepository) {
 		return (args) -> {
-			// save a few customers
-			bookRepository.save(new Book("Harry Potter", "J.K.Rowling", 20));
-			bookRepository.save(new Book("Twilight", "S.Meyer", 14));
-			bookRepository.save(new Book("The Accenture Book", "J.Sweet", 25));
-			bookRepository.save(new Book("Pride and Prejudice", "J.Austen", 15));
-			bookRepository.save(new Book("War and Peace", "L.Tolstoy", 30));
+			// save a few books
+			if (bookRepository.count() == 0) {
+				bookRepository.save(new Book("Harry Potter", "J.K.Rowling", 20));
+				bookRepository.save(new Book("Twilight", "S.Meyer", 14));
+				bookRepository.save(new Book("The Accenture Book", "J.Sweet", 25));
+				bookRepository.save(new Book("Pride and Prejudice", "J.Austen", 15));
+				bookRepository.save(new Book("War and Peace", "L.Tolstoy", 30));
 
-			// fetch all customers
-//			log.info("Customers found with findAll():");
-//			log.info("-------------------------------");
-			for (Book book : bookRepository.findAll()) {
-				System.out.println(book.toString());
+				for (Book book : bookRepository.findAll()) {
+					System.out.println(book.toString());
+				}
+				System.out.println("");
 			}
-			System.out.println("");
-
-			// fetch an individual customer by ID
-//			Customer customer = repository.findById(1L);
-//			log.info("Customer found with findById(1L):");
-//			log.info("--------------------------------");
-//			log.info(customer.toString());
-//			log.info("");
-//
-//			// fetch customers by last name
-//			log.info("Customer found with findByLastName('Bauer'):");
-//			log.info("--------------------------------------------");
-//			repository.findByLastName("Bauer").forEach(bauer -> {
-//				log.info(bauer.toString());
-//			});
-//			// for (Customer bauer : repository.findByLastName("Bauer")) {
-//			//  log.info(bauer.toString());
-//			// }
-//			log.info("");
 		};
 	}
 }
